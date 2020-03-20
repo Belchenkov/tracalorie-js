e9d504f06a23c2f7f4ba0077ee9be46041b8485c
Aleksey Belchenkov <u608110@gmail.com>
2020-03-20 14:03:04 +0300

    Delete Clear From Local Storage

--- js/app.js
+++ js/app.js
@@ -32,6 +32,31 @@ const StorageCtrl = (function () {
             }
 
             return items;
+        },
+        updateItemStorage: function (updatedItem) {
+            let items = JSON.parse(localStorage.getItem('items'));
+
+            items.forEach(function (item, index) {
+                if (updatedItem.id === item.id) {
+                    items.splice(index, 1, updatedItem);
+                }
+            });
+
+            localStorage.setItem('items', JSON.stringify(items));
+        },
+        deleteItemFromStorage: function (id) {
+            let items = JSON.parse(localStorage.getItem('items'));
+
+            items.forEach(function (item, index) {
+                if (id === item.id) {
+                    items.splice(index, 1);
+                }
+            });
+
+            localStorage.setItem('items', JSON.stringify(items));
+        },
+        clearItemsFromStorage: function () {
+            localStorage.removeItem('items');
         }
     }
 })();
@@ -376,6 +401,9 @@ const App = (function (ItemCtrl, StorageCtrl, UICtrl) {
         // Add total calories to UI
         UICtrl.showTotalCalories(totalCalories);
 
+        // Update local storage
+        StorageCtrl.updateItemStorage(updatedItem);
+
         UICtrl.clearEditState();
     };
 
@@ -394,6 +422,10 @@ const App = (function (ItemCtrl, StorageCtrl, UICtrl) {
         const totalCalories = ItemCtrl.getTotalCalories();
         // Add total calories to UI
         UICtrl.showTotalCalories(totalCalories);
+
+        // Delete from local storage
+        StorageCtrl.deleteItemFromStorage(currentItem.id);
+
         // Clear fields
         UICtrl.clearInput();
     };
@@ -411,6 +443,9 @@ const App = (function (ItemCtrl, StorageCtrl, UICtrl) {
 
         // Remove from UI
         UICtrl.removeItems();
+
+        // Clear from LS
+        StorageCtrl.clearItemsFromStorage();
 
         // Hide list
         UICtrl.hideList();
